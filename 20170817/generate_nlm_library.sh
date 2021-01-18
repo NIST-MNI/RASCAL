bbb333e82b28c8f0159416aa7966eaf1a8f0c2be
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-01-18 15:29:23 -0500

    Makig cross-validation experiment

--- 20170817/generate_nlm_library.sh
+++ 20170817/generate_nlm_library.sh
@@ -11,6 +11,6 @@ export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
 
 python -m scoop -n 32 \
    ./nist_mni_pipelines/iplScoopFusionSegmentation.py \
-   --create cerebellum_vermis_half_sc.json \
-   --output library_vermis_half
+   --create cerebellum_vermis_half_t1.json \
+   --output cb_nlm_t1
 
--- 20170817/run_cv_experiment.sh
+++ 20170817/run_cv_experiment.sh
@@ -1,22 +1,21 @@
 #! /bin/sh
 
-set -e
-set -v
+#set -e
+#set -v
 
-. /data/data01/tpmd/quarantine-1.9.15/minc-toolkit-config.sh
+#. /data/data01/tpmd/quarantine-1.9.15/minc-toolkit-config.sh
 
-PREFIX=$(pwd)
+#PREFIX=$(pwd)
 
-export PATH=$PREFIX/nist_mni_pipelines:$PATH
-export PYTHONPATH=$PREFIX/nist_mni_pipelines:$PYTHONPATH
+#export PATH=$PREFIX/nist_mni_pipelines:$PATH
+#export PYTHONPATH=$PREFIX/nist_mni_pipelines:$PYTHONPATH
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
 
-#if false ;then
-python -m scoop -vvv -n 16 \
-   $PREFIX/nist_mni_pipelines/iplScoopFusionSegmentation.py \
-   --output  cv          \
-   --segment library_vermis_half \
-   --cv      cv.json \
+python -m scoop -n 32 \
+   ./nist_mni_pipelines/ipl_segmentation_library_prepare.py \
+   --output  cv_t1     \
+   --library cb_nlm_t1 \
+   --cv      cv_t1.json \
    --options seg_options.json
-   
+
  
